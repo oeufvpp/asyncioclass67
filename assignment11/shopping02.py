2a55db9c3b22efecf4931b41b62341dcf85c2ba0
oeufvpp <oeufveerapat@gmail.com>
2024-10-19 20:12:53 +0700

    commit 19 october 2024

--- assignment11/shopping02.py
+++ assignment11/shopping02.py
@@ -1,19 +1,57 @@
-import time
+from random import random
 import asyncio
-from asyncio import Queue
-from random import randrange
-
-# we first implement the Customer and Product classes, 
-# representing customers and products that need to be checked out. 
-# The Product class has a checkout_time attribute, 
-# which represents the time required for checking out the product.
-class Product:
-    def __init__(self, product_name: str, checkout_time: float):
-        self.product_name = product_name
-        self.checkout_time = checkout_time
-
-class Customer:
-    def __init__(self, customer_id: int, products: list[Product]):
-        self.customer_id = customer_id
-        self.products = products
-
+import time  # Import time module to measure elapsed time
+ 
+# coroutine to generate work
+async def producer(queue, start_time):
+    print('Producer: Running')
+    # generate work
+    for i in range(10):
+        # generate a value
+        value = i
+        # block to simulate work
+        sleeptime = random()
+        print(f"> Producer {value} sleep {sleeptime}")
+        await asyncio.sleep(sleeptime)
+        # add to the queue
+        print(f"> Producer put {value}")
+        await queue.put(value)
+    # send an all done signal
+    await queue.put(None)
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    print(f'Producer: Done, Total time: {elapsed_time:.2f} seconds')
+ 
+# coroutine to consume work
+async def consumer(queue, start_time):
+    print('Consumer: Running')
+    # consume work
+    while True:
+        # get a unit of work without blocking
+        try:
+            item = queue.get_nowait()
+        except asyncio.QueueEmpty:
+            print('Consumer: got nothing, waiting a while...')
+            await asyncio.sleep(0.5)
+            continue
+        # check for stop
+        if item is None:
+            break
+        # report
+        print(f'\t> Consumer got {item}')
+    # all done
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    print(f'Consumer: Done, Total time: {elapsed_time:.2f} seconds')
+ 
+# entry point coroutine
+async def main():
+    # record the start time
+    start_time = time.time()
+    # create the shared queue
+    queue = asyncio.Queue()
+    # run the producer and consumers
+    await asyncio.gather(producer(queue, start_time), consumer(queue, start_time))
+  
+# start the asyncio program
+asyncio.run(main())
